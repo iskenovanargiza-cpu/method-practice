8df7882f33de5166fda02f83ef9c850d07f0aa34
novyjpolzovatel <iskenova.nargiza@gmail.com>
2025-09-26 15:57:35 -0400

    compare numbers

---
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+        System.out.println(isPositive(1000000));
+    }
+
+    public static boolean isEven(int number) {
+
+        if (number % 2 == 0)
+            return true;
+        return false;
+        }
+
+        public static boolean isPositive(double number) {
+            if (number > 0)
+                return true;
+            return false;
+        }
+
+    }
+
